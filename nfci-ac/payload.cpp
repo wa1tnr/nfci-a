c9859d522af22ca9d280fc734345e732d11d4604
wa1tnr <wa1tnr@users.noreply.github.com>
2020-10-29 16:06:46 +0000

    include FastLED Daniel Garcia V 3.3.3

--- nfci-ac/payload.cpp
+++ nfci-ac/payload.cpp
@@ -2,6 +2,7 @@
 // payload.cpp
 
 #include <Arduino.h>
+#include <FastLED.h> // FastLED by Daniel Garcia Version 3.3.3 - downloaded 29 October 2020
 
 void payload(void) { }
 // void payload(void) { Serial.println(" 'payload();' in payload.cpp reached. "); }
