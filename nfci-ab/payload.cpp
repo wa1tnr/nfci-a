d69384ec629f5c5f3111a3baac267a34b6913203
wa1tnr <wa1tnr@users.noreply.github.com>
2020-10-29 15:20:51 +0000

    Simple payload.  Hooks in place.
    	modified:   payload.cpp
    
    On branch edits-a-

--- nfci-ab/payload.cpp
+++ nfci-ab/payload.cpp
@@ -1,6 +1,9 @@
 // Thu Oct 29 15:10:30 UTC 2020
 // payload.cpp
 
+#include <Arduino.h>
+
 void payload(void) { }
+// void payload(void) { Serial.println(" 'payload();' in payload.cpp reached. "); }
 
 //END.
