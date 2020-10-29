45cbb64fd504159a49c64d08c50e0d2793ab0f93
wa1tnr <wa1tnr@users.noreply.github.com>
2020-10-29 16:09:39 +0000

    basics of FastLED setup - top of file only

--- nfci-ac/payload.cpp
+++ nfci-ac/payload.cpp
@@ -4,6 +4,11 @@
 #include <Arduino.h>
 #include <FastLED.h> // FastLED by Daniel Garcia Version 3.3.3 - downloaded 29 October 2020
 
+#define LED_PIN 8 // from human memory .. D8 maybe ;)
+#define LED_COUNT 5 // didn't count the array - has more than five for sure ;)
+
+CRGB leds[LED_COUNT]; // didn't check the lib to see which variant the CPX would prefer. ;)
+
 void payload(void) { }
 // void payload(void) { Serial.println(" 'payload();' in payload.cpp reached. "); }
 
