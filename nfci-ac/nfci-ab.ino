5c75e6968aab7335efc420a124ee7230b534d2cc
wa1tnr <wa1tnr@users.noreply.github.com>
2020-10-29 15:55:37 +0000

    Yet another copy - will begin real application dev here.
    	modified:   derived
    	new file:   nfci-ac/nfci-ab.ino
    	new file:   nfci-ac/nnfci-ab_sketch.cpp
    	new file:   nfci-ac/payload.cpp
    	new file:   nfci-ac/payload.h
    
    On branch edits-a-

---
+++ nfci-ac/nfci-ab.ino
@@ -0,0 +1,10 @@
+// Thu Oct 29 15:02:39 UTC 2020
+// file nfci-ab.ino
+
+// Thu Oct 29 13:41:38 UTC 2020
+
+// older:
+// Thu Oct 29 02:48:51 UTC 2020
+// ../../rainb/btrainb/btrainb.ino
+
+// empty file
---
+++ nfci-ac/nnfci-ab_sketch.cpp
@@ -0,0 +1,71 @@
+#include <Arduino.h>
+#include "payload.h"
+
+boolean waiting ;
+char ch;
+
+void reading(void) {
+    ch = Serial.read();
+    Serial.print(' '); Serial.print("READING ");
+    Serial.print(' '); Serial.print(" waiting is true ");
+    Serial.print(ch);
+}
+
+void is_command_waiting(void) {
+  waiting = false ;
+  if (! Serial.available()) {
+    waiting = false ;
+  } else {
+    waiting = true ;
+    reading();
+  }
+}
+
+int old_time, new_time ;
+
+#define TIME_WEIGHT 1295
+void payload_time(void) {
+  for (volatile int i = TIME_WEIGHT; i > 0; i--) {
+  }
+  Serial.print(' '); Serial.print("heart beat ..  ");
+}
+
+void noop(void) { // no op
+}
+
+#define HEART_BEAT_TIME 333
+
+void slower(void) {
+  int delta_time;
+  new_time = millis();
+  delta_time = new_time - old_time ;
+  if (delta_time > HEART_BEAT_TIME) {
+    old_time = new_time ;
+    payload_time();
+  } else {
+    noop();
+  }
+}
+
+
+void activity(void) {
+  slower();
+  payload();
+}
+
+void setup (void) {
+  waiting = false ;
+  Serial.begin(115200);
+  delay(5600);
+}
+
+void loop (void) {
+  waiting = false ;
+  while (! waiting) {
+    activity();
+    slower();
+    is_command_waiting();
+  }
+}
+
+//END.
