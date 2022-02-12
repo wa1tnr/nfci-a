4450661e1a86381a6949b610bfc711ac8bf7719f
wa1tnr <wa1tnr@users.noreply.github.com>
2022-02-12 13:41:14 +0000

    enable payload and wait blinking
    	modified:   nnfci-ae_sketch.cpp
    
    On branch edits-b-

--- nfci-ae/src/nnfci-ae_sketch.cpp
+++ nfci-ae/src/nnfci-ae_sketch.cpp
@@ -7,6 +7,15 @@
 #undef SHOW_BEAT
 #define SHOW_BEAT true
 
+#define DATE_STAMP "Sat 12 Feb 13:38:26 UTC 2022"
+
+#warning The program expects you to connect via Serial Monitor at runtime.
+// the above applies equally to any serial terminal program,
+// such as putty, hyperterm, minicom &c.
+
+// it's doing a 'while(!Serial)' loop waiting for this connection,
+// and blinks the LED (LED_BUILTIN, often on D13)while waiting.
+
 boolean waiting ;
 // int  slice; // time slice
 char ch;
@@ -62,19 +71,39 @@ void slower(void) {
 }
 
 void activity(void) {
-    // payload();
+    payload();
 }
 
 void print_login_msg(void) {
     Serial.println(" press 'b' to break    or    press 'c' to continue ");
     Serial.println("\n wrt color strobe/changes.");
-    Serial.println("\n Revision: Sat Oct 31 01:58:36 UTC 2020");
+    Serial.print("\n Revision: ");
+    Serial.println(DATE_STAMP);
+}
+
+void cpl(int pin) {
+    bool state = digitalRead(pin);
+    state = !state;
+    digitalWrite(pin, state);
+}
+
+void blink(void) {
+    cpl(LED_BUILTIN);
+    delay(50);
+    cpl(LED_BUILTIN);
+    delay(4400);
 }
 
 void setup (void) {
     waiting = false ;
     Serial.begin(115200);
-    delay(5600);
+    delay(900);
+    pinMode(LED_BUILTIN, 1); // output
+    digitalWrite(LED_BUILTIN, 0);
+    while(!Serial) {
+        blink();
+    }
+    // delay(5600);
     print_login_msg();
 
     // - - -   application (payload)   - - -
